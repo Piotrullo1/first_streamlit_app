bf0566e953ad294fd1c801215f466fa6420888dc
Piotrullo <42511940+Piotrullo1@users.noreply.github.com>
2024-01-12 16:55:17 +0100

    Update streamlit_app.py
    added example for users to pick fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
-#pick list so users can pick the fruit they want to include
-streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index))
+#pick list so users can pick the fruit they want to include         | filters data based of fruits of customer choice like an example
+streamlit.multiselect("Pick some Fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+
+
+
 
 #Display the table on plage
 streamlit.dataframe(my_fruit_list)
